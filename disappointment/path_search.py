b9d5a26ec78c9f39de53f1cc726abab8962444cd
mchodhary <mchodhary@student.unimelb.edu.au>
2022-05-01 06:01:13 +1000

    implemented a ok game agent with probabilistic move pick
    game agent picks move between normal move which makes the link move towards the goal and a capture move which is based on the strategic capture potential of a certain move

---
+++ disappointment/path_search.py
@@ -0,0 +1,188 @@
+from typing import Dict, List, Tuple, TypeVar, Optional
+import random
+from numpy import inf
+
+T = TypeVar('T')
+Location = TypeVar('Location')
+Color = TypeVar('Color')
+red = "red"
+blue = "blue"
+empty = "empty"
+
+
+class Graph:
+    """
+        adapted from https://www.redblobgames.com/pathfinding/a-star/implementation.html
+    """
+
+    def __init__(self, n, player):
+        self.player = player
+        self.enemy = red if player == blue else blue
+        self.cell: Dict[Location, List[Color, Location]] = {}
+        self.playerCell: Dict[Location, Color] = {}
+        moves = [[1, 0], [1, -1], [0, -1], [-1, 0], [-1, 1], [0, 1]]
+        for i in range(0, n):
+            for j in range(0, n):
+                curr = [i, j]
+                neighbours = []
+                color = "empty"
+                for k in moves:
+                    nCell = [0] * 2
+                    nCell[0] = curr[0] + k[0]
+                    nCell[1] = curr[1] + k[1]
+                    if not (nCell[0] < 0 or nCell[0] >= n or nCell[1] < 0 or nCell[1] >= n):
+                        neighbours.append(tuple(nCell))
+                self.cell.update({tuple(curr): [color, neighbours]})
+                # print(curr, neighbours)
+
+    def print(self):
+        for cell, neighbours in self.cell.items():
+            print(cell, neighbours)
+
+    def neighbors(self, location: Location) -> List[Location]:
+        return self.cell[location][1]
+
+    def cell_color(self, location: Location) -> Color:
+        return self.cell[location][0]
+
+    def set_cell_color(self, location: Location, color: Color):
+        if self.cell_color(location) == self.player and color != self.player:
+            # if the current id is no longer a player colour
+            self.playerCell.pop(location)
+        elif color == self.player:
+            # if the current id will be a player
+            self.playerCell.update({location: color})
+        self.cell[location][0] = color
+
+    def cost(self, src: Location, dst: Location) -> float:
+        pass
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements: List[Tuple[float, T]] = []
+
+    def empty(self) -> bool:
+        return not self.elements
+
+    def getParent(self, key) -> int:
+        if key <= 2:
+            return 0
+        else:
+            return (key - 1) // 2
+
+    def min_heapify(self, key):
+        # not used rn
+        left = (2 * key) + 1
+        right = (2 * key) + 2
+        if not right >= len(self.elements):
+            node = self.elements[key]
+            if node[0] > (self.elements[left])[0] or node[0] > self.elements[right][0]:
+                if (self.elements[right])[0] > (self.elements[left])[0]:
+                    self.elements[key], self.elements[left] = self.elements[left], self.elements[key]
+                    self.min_heapify(left)
+                else:
+                    self.elements[key], self.elements[right] = self.elements[right], self.elements[key]
+                    self.min_heapify(right)
+
+    def push(self, item: T, priority: float):
+        self.elements.append((priority, item))
+        curr = len(self.elements) - 1
+        while self.elements[curr][0] < self.elements[self.getParent(curr)][0]:
+            self.elements[curr], self.elements[self.getParent(curr)] = self.elements[self.getParent(curr)], \
+                                                                       self.elements[curr]
+            curr = self.getParent(curr)
+
+    def pop(self) -> T:
+        head = self.elements[0]
+        self.elements[0] = self.elements[-1]
+        self.elements.pop()
+        if len(self.elements) - 1 > 0:
+            self.min_heapify(0)
+        return head[1]
+        # return heapq.heappop(self.elements)[1]
+
+
+def reconstruct_path(came_from: Dict[Location, Location],
+                     start: Location, goal: Location) -> List[Location]:
+    """
+        adapted from https://www.redblobgames.com/pathfinding/a-star/implementation.html
+    """
+    current: Location = goal
+    path: List[Location] = []
+    while current != start:  # note: this will fail if no path found
+        path.append(current)
+        current = came_from[current]
+    path.append(start)  # optional
+    path.reverse()  # optional
+    return path
+
+
+def find_dist(H1, H2):
+    """
+    # adapted from redblobgames.com/grids/hexagon
+    Args:
+        H1: src hexagon cell coordinate tuple
+        H2: dst hexagon cell coordinate tuple
+
+    #Relative direction, i.e. which wedge side i need to go towards
+    1  .-'-. 0
+    2 |     | 5
+    3 '-._.-'4
+    Returns:(manhattan distance, relative direction H2 to H1)
+
+    """
+
+    r = H1[0] - H2[0]
+    q = H1[1] - H2[1]
+    s = -r - q
+    d = (abs(r) + abs(q) + abs(s)) / 2
+
+    return d
+
+
+def find_dist2(H1, goalList):
+    dist = inf
+    if not goalList:
+        return dist
+    for H2 in goalList:
+        cost = find_dist(H1, H2)
+        if cost < dist:
+            dist = cost
+
+    return dist
+
+
+def a_star_search(graph: Graph, start: Location, goal: List[Location]):
+    """
+        adapted from https://www.redblobgames.com/pathfinding/a-star/implementation.html
+    """
+    frontier = PriorityQueue()
+    frontier.push(start, 0)
+    came_from: Dict[Location, Optional[Location]] = {}
+    cost_so_far: Dict[Location, float] = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    reached_goal = False
+    endGoal: Location = []
+    while not frontier.empty():
+        current: Location = frontier.pop()
+
+        if current in goal:
+            endGoal = current
+            reached_goal = True
+            break
+
+        for next in graph.neighbors(current):
+            if graph.cell_color(next) == graph.enemy:
+                # dont want to consider enemy occupied space
+                continue
+            move_cost = 0 if graph.cell_color(next) == graph.player else 1
+            new_cost = cost_so_far[current] + move_cost  # +1 for cost to next node
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + find_dist2(next, goal)
+                frontier.push(next, priority)
+                came_from[next] = current
+
+    return reached_goal, came_from, cost_so_far, endGoal
--- disappointment/player.py
+++ disappointment/player.py
@@ -1,3 +1,19 @@
+from path_search import *
+from numpy import random
+
+POSSIBLE_DIAMONDS = [[(2, -1), (0, 0), (1, -1), (1, 0)], [(0, 0), (-2, 1), (-1, 0), (-1, 1)],
+                     [(1, -1), (-1, 0), (0, -1), (0, 0)], [(1, 0), (-1, 1), (0, 1), (0, 0)],
+                     [(1, 0), (0, 0), (1, -1), (0, 1)], [(0, 0), (-1, 0), (0, -1), (-1, 1)],
+                     [(1, -1), (0, -1), (1, -2), (0, 0)], [(0, 1), (-1, 1), (0, 0), (-1, 2)],
+                     [(1, -1), (0, 0), (0, -1), (1, 0)], [(0, 0), (-1, 1), (-1, 0), (0, 1)],
+                     [(0, -1), (-1, 0), (-1, -1), (0, 0)], [(1, 0), (0, 1), (0, 0), (1, 1)]]
+POSSIBLE_DIAMONDS2 = [[(0, 0), (2, -1), (1, -1), (1, 0)], [(0, 0), (-2, 1), (-1, 0), (-1, 1)],
+                      [(0, 0), (0, -1), (1, -1), (-1, 0)], [(0, 0), (0, 1), (1, 0), (-1, 1)],
+                      [(0, 0), (1, 0), (1, -1), (0, 1)], [(0, 0), (-1, 0), (0, -1), (-1, 1)],
+                      [(0, 0), (1, -2), (1, -1), (0, -1)], [(0, 0), (-1, 2), (0, 1), (-1, 1)],
+                      [(0, 0), (1, -1), (0, -1), (1, 0)], [(0, 0), (-1, 1), (-1, 0), (0, 1)],
+                      [(0, 0), (-1, -1), (0, -1), (-1, 0)], [(0, 0), (1, 1), (1, 0), (0, 1)]]
+
 
 class Player:
     def __init__(self, player, n):
@@ -9,25 +25,182 @@ class Player:
         play as Red, or the string "blue" if your player will play
         as Blue.
         """
-        # put your code here
+        self.color = player
+        self.n = n
+        self.gameState = Graph(n, player)
+        self.begin: List[Location] = []
+        self.goal: List[Location] = []
+        self.prevMove: Location = []
+        self.currMove: Location = []
+        self.nlinkedPath = 0
+        self.prevLinkedPath = 0
+        if player == red:
+            for i in range(0, n):
+                nCell = [0] * 2
+                nCell[0] = 0
+                nCell[1] = i
+                self.begin.append(tuple(nCell))
+                gCell = [0] * 2
+                gCell[0] = n - 1
+                gCell[1] = i
+                self.goal.append(tuple(gCell))
+        else:
+            self.goal = []
+            for i in range(0, n):
+                nCell = [0] * 2
+                nCell[0] = i
+                nCell[1] = 0
+                self.begin.append(tuple(nCell))
+                gCell = [0] * 2
+                gCell[0] = i
+                gCell[1] = n - 1
+                self.goal.append(tuple(gCell))
 
     def action(self):
         """
         Called at the beginning of your turn. Based on the current state
         of the game, select an action to play.
         """
-        # put your code here
-    
+        self.prevMove = self.currMove
+        location: Location = self.get_next_move()
+        print(self.prevMove)
+        print(location)
+        self.currMove = location
+        return (str("PLACE"), int(location[0]), int(location[1]))
+
     def turn(self, player, action):
         """
-        Called at the end of each player's turn to inform this player of 
-        their chosen action. Update your internal representation of the 
-        game state based on this. The parameter action is the chosen 
-        action itself. 
-        
+        Called at the end of each player's turn to inform this player of
+        their chosen action. Update your internal representation of the
+        game state based on this. The parameter action is the chosen
+        action itself.
+
         Note: At the end of your player's turn, the action parameter is
         the same as what your player returned from the action method
         above. However, the referee has validated it at this point.
         """
-        # put your code here
+        cell = [0] * 2
+        cell[0] = action[1]
+        cell[1] = action[2]
+        self.gameState.set_cell_color(tuple(cell), player)
+        self.check_diamond(cell[0], cell[1], player)
+        return
 
+    def check_diamond(self, r, c, player):
+        to_clear = []
+        for cells in self.generate_valid_diamonds(r, c):
+            up = self.gameState.cell_color(cells[0])
+            down = self.gameState.cell_color(cells[1])
+            left = self.gameState.cell_color(cells[2])
+            right = self.gameState.cell_color(cells[3])
+            if up == down and left == right and up != left:
+                if up == player or down == player:
+                    to_clear.append(cells[2])
+                    to_clear.append(cells[3])
+                if left == player or right == player:
+                    to_clear.append(cells[0])
+                    to_clear.append(cells[1])
+        for cell in to_clear:
+            self.gameState.set_cell_color(cell, empty)
+
+    def generate_valid_diamonds(self, r, c):
+        valid_diamonds = []
+        for cells in POSSIBLE_DIAMONDS2:
+            valid_diamond = []
+            invalid = False
+            for (x, y) in cells:
+                if 0 <= (x + r) < self.n and 0 <= (y + c) < self.n:
+                    valid_diamond.append((x + r, y + c))
+                else:
+                    invalid = True
+                    break
+            if not invalid:
+                valid_diamonds.append(valid_diamond)
+        return valid_diamonds
+
+    def get_next_move(self) -> Location:
+        cost = inf
+        reachedGoal = False
+        path: List[Location] = []
+        move: Location = []
+        location: Location = []
+        location1: Location = []
+        for start in self.begin:
+            if self.gameState.cell_color(start) == self.gameState.enemy:
+                continue
+            reached_goal, came_from, cost_so_far, endGoal = a_star_search(self.gameState, start, self.goal)
+            if not reached_goal:
+                continue
+            else:
+                reachedGoal = True
+                if cost_so_far[endGoal] < cost:
+                    cost = cost_so_far[endGoal]
+                    path = reconstruct_path(came_from, start, endGoal)
+        print(path)
+        if reachedGoal:
+            self.prevLinkedPath = self.nlinkedPath
+            self.nlinkedPath = 0
+            for cell in path:
+                if self.gameState.cell_color(cell) == self.color:
+                    self.nlinkedPath += 1
+                if self.gameState.cell_color(cell) == empty:
+                    location = cell
+                    break
+        location1 = self.find_capture_move()
+        if not location1:
+            move = location
+        else:
+            """
+                TO-DO
+                at the start of game or based on number of pieces on the board set higher probability for normal a* move
+                then slowly increase the probability of capture move
+            """
+            p_location = random.uniform(0.8, 0.2)
+            choice = [0, 1]
+            randomMove = random.choice(choice, 1, p=[p_location, 1 - p_location])
+            print("random move: ", p_location)
+            print(randomMove)
+            if randomMove == choice[0]:
+                move = location
+            elif randomMove == choice[1]:
+                move = location1
+            print("location ", end=' ')
+            print(location, end=' ')
+            print("location1 ", end=' ')
+            print(location1)
+            print(move)
+        return move
+
+    def find_capture_move(self) -> Location:
+        move: Location = []
+        nCaptures = 0
+        for cell in self.gameState.playerCell:
+            r = cell[0]
+            c = cell[1]
+            cnt = 0
+            for cells in self.generate_valid_diamonds(r, c):
+                up = self.gameState.cell_color(cells[0])
+                down = self.gameState.cell_color(cells[1])
+                left = self.gameState.cell_color(cells[2])
+                right = self.gameState.cell_color(cells[3])
+                if down == empty and left == right and up != left:
+                    cnt = self.move_capture_potential(cells[1])
+                    if cnt > nCaptures:
+                        nCaptures = cnt
+                        move = cells[1]
+        print('capture potential', end=' ')
+        print(nCaptures)
+        return move
+
+    def move_capture_potential(self, location: Location):
+        r = location[0]
+        c = location[1]
+        cnt = 0
+        for cells in self.generate_valid_diamonds(r, c):
+            up = self.gameState.cell_color(cells[0])
+            down = self.gameState.cell_color(cells[1])
+            left = self.gameState.cell_color(cells[2])
+            right = self.gameState.cell_color(cells[3])
+            if up == empty and left == right and down != left and down != up:
+                cnt += 1
+        return cnt
