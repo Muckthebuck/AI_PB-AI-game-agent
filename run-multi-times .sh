f9a242cd27059b472e9d07eddde8d5fbb856ae3c
Aimee <ayliang@student.unimelb.edu.au>
2022-05-10 23:28:01 +1000

    updated

--- run-multi-times .sh
+++ run-multi-times .sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-run_times = 2
+run_times =2
 
 echo "Going to run model for ${run_times} time(s)"
 for k in $(seq 1 ${run_times})
 do
-  python -m referee 5 disappointment disappointment -l game${k}
+  python -m referee 7 emotionaldamage disappointment -l game${k}
 done
